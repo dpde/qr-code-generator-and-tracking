b47d84b852421b088499c8bbe5297f417e6cd876
Adam Parusel <me@adamparusel.com>
2024-07-28 23:04:02 +0200

    Add back default Laravel home page

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RedirectController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/redirect/{targetPage}', [RedirectController::class, 'trackAndRedirect'])->name('trackAndRedirect');
